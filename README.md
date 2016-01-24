fe4f4e9dafbdc54628cf851ddaaff30d7ac765e2
Alex Swan <metal10k@hotmail.com>
2016-01-24 13:00:55 +0000

    Added filtering and undo. Created global references

---
+++ README.md
@@ -0,0 +1,35 @@
+### Javascript event store
+
+This is a simple cli tool to allow storing of arbitrary events in a json file.
+
+To install:
+
+> npm install js-event-store-cli -g
+
+Then you can record events from any folder:
+
+> record-event hello world
+
+> record-event something:42 somethingelse:21 someArray:2:3:5:8 someString:aValue
+
+A db.json file will be created in the executing folder if it does not yet exist.
+This is the local database used to store and retrieve events.
+
+It is also possible to undo the last event
+
+> undo-last-event
+
+It is also possible to do some simple filtering operations. This is a work in progress.
+
+All events where a property exists:
+
+>filter-events someProperty
+
+All events where the property equals the specified value
+
+>filter-events something=42
+
+>filter-events somethingelse=true
+
+
+
--- scripts/lib/parser.js
+++ scripts/lib/parser.js
@@ -1,3 +1,11 @@
+function transposeInput(input){
+    if(input === "true" || input === "True" || input === "TRUE")
+        return true;
+    else if(input === "false" || input === "False" || input === "FALSE")
+        return false;
+    return input;
+}
+
 module.exports = {
    parse : function(inputs){ //string[]
         var data = inputs.map(function(input){
@@ -22,5 +30,46 @@ module.exports = {
         }, {});
         
         return dataAsObj;
+    },
+    parseFilters: function(inputs){ //string[]
+        var data = inputs.map(function(input){
+            var clauses = input.split("&");
+            return clauses.map(function(clause){
+               var s = clause.split("=");
+               var left = s[0];
+               var right = s[1];
+               return function(input){
+                   //console.log(input);
+                   var data = input.data;
+                   if(!data)
+                        return false;
+                   
+                //    console.log("record for :" + JSON.stringify(input));
+                    //console.log(transposeInput(data[left]));
+                //    console.log(right);
+                    //console.log(transposeInput(data[left]) == right);
+                    
+                   if(!right)
+                       if(data[left])
+                            return true;
+                       else
+                            return false
+                    
+                   return data[left] == transposeInput(right);
+               }
+            }).reduce(function(acc, item){
+                return function(input){
+                    //console.log(input);
+                    //console.log(item(input));
+                    return acc(input) && item(input);
+                };
+            }, function(input){return true;});
+        }).reduce(function(acc, item){
+                return function(input){
+                    //console.log(input);
+                    return acc(input) && item(input);
+                };
+            }, function(input){return true;});
+        return data;
     }
 }
